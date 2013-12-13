0a1723328473e72ea2fa7cb26c721a237edb122d
Sergey (scor) Bubnov <omgbebebe@gmail.com>
2013-12-13 15:49:32 +0400

    install openstack kernel and iproute with namespaces

--- chef/cookbooks/neutron/recipes/common_install.rb
+++ chef/cookbooks/neutron/recipes/common_install.rb
@@ -463,7 +463,7 @@ end
 
 
 if %w(redhat centos).include?(node.platform)
-  net_core_pkgs=%w(kernel iproute iputils)
+  net_core_pkgs=%w(kernel-*openstack* iproute-*el6ost.netns* iputils)
 
   ruby_block "unset_reboot" do
     block do
@@ -483,8 +483,9 @@ if %w(redhat centos).include?(node.platform)
   end
 
   net_core_pkgs.each do |pkg|
-    package "#{pkg}" do
-      action :upgrade
+    bash "install net pkgs" do
+      user "root"
+      code "yum install #{pkg}"
       notifies :create, "ruby_block[set_reboot]"
     end
   end
