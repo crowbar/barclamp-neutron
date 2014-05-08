b0b117efc2d87bf327d6a1ee362fbddc2fafe514
Thomas Boerger <tboerger@suse.de>
2014-05-08 10:37:09 +0200

    Reafctored most parts and did some unification

--- crowbar_framework/lib/crowbar/registry/neutron.rb
+++ crowbar_framework/lib/crowbar/registry/neutron.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+# -*- encoding : utf-8 -*-
 #
 # Copyright 2011-2013, Dell
 # Copyright 2013-2014, SUSE LINUX Products GmbH
@@ -16,14 +16,10 @@
 # limitations under the License.
 #
 
-#
-#
-# TODO: Needs to be replaced by https://github.com/basecamp/sub
-#
-#
+module Crowbar
+  module Registry
+    class Neutron < Base
 
-require File.expand_path("../barclamp_lib", __FILE__)
-@barclamp = "neutron"
-@timeout = 3600
-
-main
+    end
+  end
+end
